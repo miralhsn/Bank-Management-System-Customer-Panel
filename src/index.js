4dac72a7c335ecf0e65a07c57f287a2c28c55c3d
Miral <miralhsn>
2024-12-04 19:55:10 +0500

    File structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import express from 'express';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js'; // Add `.js` here
 import './index.css';
-import userRouter from './routes/user'; 
 
-
-const app = express();
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,7 +11,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-app.use('/user', userRouter);
-
-app.listen(5000, () => console.log('Server running on port 5000'));
