d8a7cdcef05ff2b9da131d0e64ade9cbef4d2b93
Miral <miralhsn>
2024-12-04 19:29:58 +0500

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Bell, User } from 'lucide-react';
-import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -12,31 +11,14 @@ function Navbar() {
             <span className="text-2xl font-bold text-blue-600">ReliPay</span>
           </div>
 
-          {/* Navbar Links */}
-          <div className="hidden md:flex space-x-6">
-            <NavLink to="/" className="text-gray-600 hover:text-blue-600" activeClassName="text-blue-600 font-semibold">
-              Dashboard
-            </NavLink>
-            <NavLink to="/transactions" className="text-gray-600 hover:text-blue-600" activeClassName="text-blue-600 font-semibold">
-              Transactions
-            </NavLink>
-            <NavLink to="/transfer" className="text-gray-600 hover:text-blue-600" activeClassName="text-blue-600 font-semibold">
-              Fund Transfer
-            </NavLink>
-            <NavLink to="/loans" className="text-gray-600 hover:text-blue-600" activeClassName="text-blue-600 font-semibold">
-              Loans
-            </NavLink>
-            <NavLink to="/profile" className="text-gray-600 hover:text-blue-600" activeClassName="text-blue-600 font-semibold">
-              Profile
-            </NavLink>
-          </div>
-
-          {/* Notification and User */}
+          {/* Notification and User Section */}
           <div className="flex items-center space-x-4">
+            {/* Notification Icon */}
             <button className="p-2 rounded-full hover:bg-gray-100">
               <Bell className="h-6 w-6 text-gray-600" />
             </button>
 
+            {/* User Section */}
             <div className="flex items-center space-x-2">
               <div className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center">
                 <User className="h-5 w-5 text-blue-600" />
